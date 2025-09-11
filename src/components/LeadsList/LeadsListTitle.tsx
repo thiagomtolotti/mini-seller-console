871cd52d5aba044a39f081c67e24b7a4697ec317
Thiago Tolotti <thiagotolotti@gmail.com>
2025-09-11 20:15:51 -0300

    adds LeadsListTitle

---
+++ src/components/LeadsList/LeadsListTitle.tsx
@@ -0,0 +1,3 @@
+export default function LeadsListTitle() {
+  return <h1 className="text-3xl font-bold mb-6 text-slate-900">Leads</h1>;
+}
--- src/components/LeadsList/index.tsx
+++ src/components/LeadsList/index.tsx
@@ -4,9 +4,11 @@ import { LeadsListProvider } from "@/contexts/LeadsListProvider";
 
 import type { Lead } from "@/types/lead";
 
-import LeadDetailPanel from "../LeadDetailPanel";
+import LeadsListTitle from "./LeadsListTitle";
+
 import LeadsListTableFilters from "./LeadsListFilters";
 import LeadsListTable from "./LeadsListTable";
+import LeadDetailPanel from "../LeadDetailPanel";
 
 export default function LeadsList() {
   const [selectedLead, setSelectedLead] = useState<Lead | null>(null);
@@ -14,6 +16,8 @@ export default function LeadsList() {
   return (
     <LeadsListProvider>
       <div className="mx-auto w-full max-w-4xl bg-slate-50 p-10 m-12 rounded-xl drop-shadow-lg text-slate-800">
+        <LeadsListTitle />
+
         <LeadsListTableFilters />
 
         <LeadsListTable selectLead={setSelectedLead} />
