2b58e7e4acb4405413faa0eb66f48f8ff6323a20
Thiago Tolotti <thiagotolotti@gmail.com>
2025-09-28 15:41:33 -0300

    adds coverage threshold

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,16 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     globals: true,
-    coverage: { provider: "istanbul" },
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "html"],
+      thresholds: {
+        statements: 90,
+        branches: 90,
+        functions: 90,
+        lines: 90,
+      },
+    },
     environment: "jsdom",
     setupFiles: "./tests/setup.ts",
   },
